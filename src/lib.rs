b757a2b9bd62a5928cb29f9feb8ea7c517ccdd1e
Ho Vei <noelwei@gmail.com>
2022-09-14 23:20:28 +0800

    better structure

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,64 @@
 
 pub mod hash;
 pub mod poseidon;
+pub use hash::Hashable;
+pub use halo2_proofs::pairing::bn256::Fr as Bn256Fr;
+
+#[cfg(test)]
+mod tests {
+    use super::hash::*;
+    use super::Bn256Fr as Fr;
+    use ff::PrimeField;
+
+    #[test]
+    fn poseidon_hash() {
+        let b1: Fr = Fr::from_str_vartime("1").unwrap();
+        let b2: Fr = Fr::from_str_vartime("2").unwrap();
+
+        let h = Fr::hash([b1, b2]);
+        assert_eq!(
+            h.to_string(),
+            "0x115cc0f5e7d690413df64c6b9662e9cf2a3617f2743245519e19607a4417189a" // "7853200120776062878684798364095072458815029376092732009249414926327459813530"
+        );
+    }
+
+    use halo2_proofs::dev::MockProver;
+
+    #[cfg(feature = "print_layout")]
+    #[test]
+    fn print_circuit() {
+        use plotters::prelude::*;
+
+        let root = BitMapBackend::new("layouts/hash-layout.png", (1024, 768)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+        let root = root
+            .titled("Hash circuit Layout", ("sans-serif", 60))
+            .unwrap();
+
+        let circuit = HashCircuit::<Fr> {
+            calcs: 1,
+            ..Default::default()
+        };
+        halo2_proofs::dev::CircuitLayout::default()
+            .show_equality_constraints(true)
+            .render(6, &circuit, &root)
+            .unwrap();
+    }
+
+    #[test]
+    fn poseidon_hash_circuit() {
+        let message = [
+            Fr::from_str_vartime("1").unwrap(),
+            Fr::from_str_vartime("2").unwrap(),
+        ];
+
+        let k = 6;
+        let circuit = HashCircuit::<Fr> {
+            calcs: 1,
+            inputs: vec![message],
+            ..Default::default()
+        };
+        let prover = MockProver::run(k, &circuit, vec![]).unwrap();
+        assert_eq!(prover.verify(), Ok(()));
+    }
+}
