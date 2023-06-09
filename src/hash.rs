784c2794793733c6388a9eadb51805254cdd0eda
Alan WANG <alan1995wang@outlook.com>
2023-06-10 05:56:14 +0800

    feat: merge 75a2d04

--- src/hash.rs
+++ src/hash.rs
@@ -652,11 +652,14 @@ impl<'d, F: Hashable, const STEP: usize, PC: PermuteChip<F, F::SpecType, 3, 2>>
             },
         )?;
 
+        layouter.assign_region(|| "hash table custom", |mut region| {
+            self.fill_hash_tbl_custom(&mut region)
+        })?;
+
         let (states_in, states_out) = layouter.assign_region(
             || "hash table",
             |mut region| {
-                let offset = self.fill_hash_tbl_custom(&mut region)?;
-                self.fill_hash_tbl_body(&mut region, offset)
+                self.fill_hash_tbl_body(&mut region, 0)
             },
         )?;
 
