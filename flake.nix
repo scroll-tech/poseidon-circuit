1a5b02b65f8ebb2c2f286d6c1cd4cbd1b491cab4
Cameron <cameron.studdstreet@gmail.com>
2023-12-21 09:31:57 +0000

    compile on stable

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Basic devshell for polybase";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        };
+
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ 
+            rustToolchain
+            pkg-config
+            fontconfig
+          ];
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+        };
+      });
+}
