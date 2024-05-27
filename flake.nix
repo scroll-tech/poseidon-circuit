1d399a4e411275be321a4d2a4c91904d47bd79b5
Cameron <cameron.studdstreet@gmail.com>
2024-05-27 18:56:48 +0100

    format flake

--- flake.nix
+++ flake.nix
@@ -6,26 +6,29 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [(import rust-overlay)];
+      };
 
-        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" "rust-analyzer" ];
-        };
-
-      in {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [ 
-            rustToolchain
-            pkg-config
-            fontconfig
-          ];
-          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
-        };
-      });
+      rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+        extensions = ["rust-src" "rust-analyzer"];
+      };
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          rustToolchain
+          pkg-config
+          fontconfig
+        ];
+        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+      };
+    });
 }
